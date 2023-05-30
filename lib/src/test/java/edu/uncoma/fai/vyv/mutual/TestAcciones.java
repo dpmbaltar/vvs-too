06fc43cabc54836c744e108e025e0388ecb74827
Diego Pablo Matías Baltar <dpmbaltar@gmail.com>
2023-05-30 18:35:24 -0300

    Descomentar aserciones

--- lib/src/test/java/edu/uncoma/fai/vyv/mutual/TestAcciones.java
+++ lib/src/test/java/edu/uncoma/fai/vyv/mutual/TestAcciones.java
@@ -128,7 +128,7 @@ public class TestAcciones {
         assertTrue(1500 == m.getCredito());
         assertTrue(0 == m.getConsumo());
         assertTrue(0 == m.getBeneficiarios());
-        //assertTrue(3 == m.getTitulares());
+        assertTrue(3 == m.getTitulares());
         assertTrue(Mutual.ESTADO_GANANCIA == m.getEstado());
 
         // paso 16
@@ -136,7 +136,7 @@ public class TestAcciones {
         assertTrue(1300 == m.getCredito());
         assertTrue(0 == m.getConsumo());
         assertTrue(0 == m.getBeneficiarios());
-        //assertTrue(2 == m.getTitulares());
+        assertTrue(2 == m.getTitulares());
         assertTrue(Mutual.ESTADO_GANANCIA == m.getEstado());
 
         // paso 17
@@ -144,7 +144,7 @@ public class TestAcciones {
         assertTrue(900 == m.getCredito());
         assertTrue(0 == m.getConsumo());
         assertTrue(0 == m.getBeneficiarios());
-        //assertTrue(1 == m.getTitulares());
+        assertTrue(1 == m.getTitulares());
         assertTrue(Mutual.ESTADO_GANANCIA == m.getEstado());
 
         // paso 18
@@ -160,7 +160,7 @@ public class TestAcciones {
         assertTrue(400 == m.getCredito());
         assertTrue(0 == m.getConsumo());
         assertTrue(0 == m.getBeneficiarios());
-        //assertTrue(0 == m.getTitulares());
+        assertTrue(0 == m.getTitulares());
         assertTrue(Mutual.ESTADO_GANANCIA == m.getEstado());
     }
 
